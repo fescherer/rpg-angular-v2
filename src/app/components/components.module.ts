25ccf21e7760722adde01a199b76bc89a6f76b30
Felipe Scherer <felipescherer25@gmail.com>
2022-06-29 22:18:08 -0300

    feat: player traits

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PlayerTraitsComponent } from './player-traits/player-traits.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [FooterComponent, HeaderComponent, PlayerTraitsComponent],
   imports: [CommonModule],
-  exports: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent, FormsModule, ReactiveFormsModule, PlayerTraitsComponent],
 })
 export class ComponentsModule {}
---
+++ src/app/components/player-traits/player-traits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-traits',
+  templateUrl: './player-traits.component.html',
+  styleUrls: ['./player-traits.component.scss'],
+})
+export class PlayerTraitsComponent {
+  constructor() {}
+}
