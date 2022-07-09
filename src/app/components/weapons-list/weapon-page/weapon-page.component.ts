6a832ac4a0b850bad2404344b113898dea3729f0
Felipe Scherer <felipescherer25@gmail.com>
2022-07-09 15:30:59 -0300

    feat: weapon page

--- src/app/components/weapons-list/weapon-page/weapon-page.component.ts
+++ src/app/components/weapons-list/weapon-page/weapon-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-weapon-page',
@@ -6,5 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weapon-page.component.scss'],
 })
 export class WeaponPageComponent {
+  weaponForm = new FormGroup({
+    name: new FormControl(''),
+    url: new FormControl(''),
+    description: new FormControl(''),
+    damage: new FormControl(''),
+  });
+
   constructor() {}
 }
