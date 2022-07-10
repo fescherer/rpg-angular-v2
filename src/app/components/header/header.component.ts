97c9332a2c310c3cd4c56123693c2de910695bef
Felipe Scherer <felipescherer25@gmail.com>
2022-07-10 01:20:56 -0300

    feat: add complicated menus

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  theme: string | null = document.documentElement.getAttribute('data-theme') || 'light';
   constructor() {}
 
   changeTheme(): void {
     let theme = document.documentElement.getAttribute('data-theme');
     document.documentElement.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
+    this.theme = theme === 'dark' ? 'light' : 'dark';
+  }
+
+  openNav() {
+    // @ts-ignore
+    document.getElementById('mySidenav').style.width = '250px';
+  }
+
+  openNav2() {
+    // @ts-ignore
+    document.getElementById('mySidenav').style.display = 'flex';
+    // @ts-ignore
+    document.getElementById('mySidenav').style.zIndex = 20;
+    // @ts-ignore
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  closeNav() {
+    // @ts-ignore
+    document.getElementById('mySidenav').style.width = '0';
+  }
+
+  closeNav2() {
+    // @ts-ignore
+    document.getElementById('mySidenav').style.display = 'none';
+    // @ts-ignore
+    document.getElementById('mySidenav').style.zIndex = 1;
+    // @ts-ignore
+    document.getElementById('overlay').style.display = 'none';
   }
 }
--- src/app/rpg-angular/body/character-sheet/character-sheet.component.ts
+++ src/app/rpg-angular/body/character-sheet/character-sheet.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class CharacterSheetComponent {
   constructor() {}
+
+  scrollTo(element: any): void {
+    (document.getElementById(element) as HTMLElement).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
 }
