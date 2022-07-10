ea5a1f47d13f2df0048c159ed86a46a1bc70cd8e
Felipe Scherer <felipescherer25@gmail.com>
2022-07-10 16:32:38 -0300

    feat: firebase service

--- src/services/firestore.service.ts
+++ src/services/firestore.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-// import { rejects } from 'assert';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
 
 import {
   Firestore,
   doc,
-  onSnapshot,
   DocumentReference,
-  docSnapshots,
   setDoc,
   CollectionReference,
   collection,
-  addDoc,
   docData,
   updateDoc,
   deleteDoc,
@@ -26,12 +21,6 @@ export class FirestoreService {
   collection!: CollectionReference;
   constructor(private firestore: Firestore, private toastr: ToastrService) {
     this.collection = collection(firestore, 'page');
-    // this.doc = doc(firestore, 'page');
-    // onSnapshot(doc, snap => {
-
-    // });
-
-    // docSnapshots(doc).subscribe(...);
   }
 
   async getsheet() {
@@ -50,55 +39,24 @@ export class FirestoreService {
   }
 
   async update() {
-    await updateDoc(doc(this.collection, 'aaaa'), { id: 'felipe scherer gostoso' });
+    await updateDoc(doc(this.collection, 'aaaa'), { id: 'felipe scherer gostoso' })
+      .then(() => {
+        this.toastr.success('Ficha atualizada', 'Parabéns!');
+      })
+      .catch((err) => {
+        console.log(err);
+        this.toastr.error('Erro na atualização da ficha', 'Algo deu errado!');
+      });
   }
 
   async delete() {
-    await deleteDoc(doc(this.collection, 'aaaa'));
+    await deleteDoc(doc(this.collection, 'aaaa'))
+      .then(() => {
+        this.toastr.error('Até a próxima', 'Tchau ficha!');
+      })
+      .catch((err) => {
+        console.log(err);
+        this.toastr.warning('Erro na deleção da ficha', 'Algo deu errado!');
+      });
   }
-
-  // async createCharacterSheet(data: any) {
-  //   const docRef = doc(this.firestore, 'cities', 'SF');
-  //   const docSnap = await getDoc(docRef);
-
-  //   return new Promise<any>((resolve, reject) => {
-  //     this.angularFire
-  //       .collection('page')
-  //       .doc(data.id)
-  //       .set(data)
-  //       .then(
-  //         (res) => {
-  //           this.toastr.success('Ficha criada', 'Parabéns!');
-  //         },
-  //         (err) => {
-  //           console.log(err);
-  //           this.toastr.error('Erro na criação da ficha', 'Algo deu errado!');
-  //         },
-  //       );
-  //   });
-  // }
-
-  // updateCharacterSheet(page: any) {
-  //   this.angularFire
-  //     .collection('page')
-  //     .doc(`${page.id}`)
-  //     .update(page)
-  //     .then(
-  //       (res) => {
-  //         this.toastr.success('Ficha atualizada', 'Parabéns!');
-  //       },
-  //       (err) => {
-  //         this.toastr.success('Erro na atualização da ficha', 'Algo deu errado!');
-  //       },
-  //     );
-  // }
-
-  // deleteCharacterSheet(data: any) {
-  //   this.angularFire.doc(`page/${data.id}`).delete();
-  //   this.toastr.success('Até a próxima', 'Tchau ficha!');
-  // }
-
-  // getCharacterSheetList(page?: any) {
-  //   return this.angularFire.collection('page').doc(`${page.id}`).valueChanges();
-  // }
 }
